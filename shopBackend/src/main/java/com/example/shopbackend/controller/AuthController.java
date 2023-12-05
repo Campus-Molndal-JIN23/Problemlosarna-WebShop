b2e9152af88b26ce22fc23c3fc8f8e42a85e72c1
Wakana <83waksug@gafe.molndal.se>
2023-12-05 12:12:40 +0100

    /webshop/Auth for test

--- shopBackend/src/main/java/com/example/shopbackend/controller/AuthController.java
+++ shopBackend/src/main/java/com/example/shopbackend/controller/AuthController.java
@@ -1,6 +1,48 @@
 package com.example.shopbackend.controller;
 
+import ch.qos.logback.core.model.Model;
+import com.example.shopbackend.form.LoginForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/webshop/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    @GetMapping("/register")
+    public String registerView(Model model, LoginForm loginForm){
+
+
+        return "register"; //Todo change to actual html name
+    }
+
+    @PostMapping("/register")
+    public void register(Model model, LoginForm loginForm){
+
+        System.out.println("Username: " + loginForm.getUsername());
+        System.out.println("Email: " + loginForm.getPassword());
+
+    }
+
+
+    @GetMapping("/login")
+    public String loginView(Model model,LoginForm loginForm){
+
+        return "login"; //Todo change to actual html name
+    }
+
+    @PostMapping("/login")
+    public void login(Model model,LoginForm loginForm ){
+
+        System.out.println(loginForm.getPassword());
+        System.out.println(loginForm.getUsername());
+
+        System.out.println("JWT token genererad");
+
+    }
+
 
 
 }
+
---
+++ shopBackend/src/main/java/com/example/shopbackend/form/LoginForm.java
@@ -0,0 +1,13 @@
+package com.example.shopbackend.form;
+
+import lombok.Data;
+
+@Data
+public class LoginForm {
+
+    /**  login id*/
+    String username;
+    /**  password */
+    String password;
+
+}
