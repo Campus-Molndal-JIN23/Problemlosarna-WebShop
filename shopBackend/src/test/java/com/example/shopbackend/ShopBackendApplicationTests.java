384f3a3468895c241f969b7a06239cf161d4bb6f
Emilsivertsson <Emil.sivertsson@gmail.com>
2023-12-05 09:06:02 +0100

    added backend module

---
+++ shopBackend/src/test/java/com/example/shopbackend/ShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
