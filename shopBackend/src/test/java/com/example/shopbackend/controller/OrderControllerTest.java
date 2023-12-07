7f72a4e003ddaaa6b4c56a41769e7b4eb61bd967
Wakana <83waksug@gafe.molndal.se>
2023-12-07 15:57:24 +0100

    /webshop/order for test

--- shopBackend/src/test/java/com/example/shopbackend/controller/OrderControllerTest.java
+++ shopBackend/src/test/java/com/example/shopbackend/controller/OrderControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.shopbackend.controller;
 
+import com.example.shopbackend.entity.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashMap;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,17 +27,27 @@ class OrderControllerTest {
 
     @Test
     void order() throws Exception {
+       User user = new User();
+
         this.mvc.perform(post(API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(user)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getOrder() throws Exception {
+        this.mvc.perform(get(API)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getOrders() throws Exception {
+
+        this.mvc.perform(get(API)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
-    }
-
-    @Test
-    void getOrder() {
-    }
-
-    @Test
-    void getOrders() {
     }
 }
