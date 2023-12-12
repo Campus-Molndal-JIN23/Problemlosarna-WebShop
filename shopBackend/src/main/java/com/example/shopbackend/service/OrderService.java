391dafc00088521d17b1f5ddaa87357b88bea9a0
Kristian Karlson <83krikar@gafe.molndal.se>
2023-12-12 16:03:04 +0100

    starting order object

--- shopBackend/src/main/java/com/example/shopbackend/service/OrderService.java
+++ shopBackend/src/main/java/com/example/shopbackend/service/OrderService.java
@@ -1,4 +1,30 @@
 package com.example.shopbackend.service;
 
+import com.example.shopbackend.entity.Order;
+import com.example.shopbackend.repository.OrderQtyRepository;
+import com.example.shopbackend.repository.OrderRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    private final OrderQtyRepository orderQtyRepository;
+
+    public OrderService(OrderRepository orderRepository, OrderQtyRepository orderQtyRepository) {
+        this.orderRepository = orderRepository;
+        this.orderQtyRepository = orderQtyRepository;
+    }
+
+    public List<Object> findAllUserOrders(Long userId) {
+
+        Optional<List<Order>> orders = orderRepository.findByUserId(userId);
+
+        OrderDTO userOrders = new OrderDTO(orders);
+
+        return null;
+    }
+
 }
