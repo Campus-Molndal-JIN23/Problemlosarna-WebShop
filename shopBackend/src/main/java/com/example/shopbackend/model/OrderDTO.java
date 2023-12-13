5d22fb6f125d3cbd3883ffe1a488b1b5e3d1e26d
Kristian Karlson <sppdc1@gmail.com>
2023-12-13 09:32:56 +0100

    id added to products in basket
    * can handle basket and orders
    
    * updated product controller & service test
    
    * DTO is there bont wont pass tru the controller
    
    * get basketController works and sum is correct
    
    * refactor
    
    * bring latest version to branch (#34)
    
    * Think i understand somewhat how Frontend-User supposed to work.
    
    Looked through UserController and BasketHttp for how to work, and TODO:s,
    
     and polished the tables a little bit so look graphically a little bit more done.
    
    * Update ApiContract.md
    
    * Added Badge, Credits and License to Readme.
    
    * Basket ctrl service, get basket in proper format but will allways return the same basket (#33)
    
    * can handle basket and orders
    
    * updated product controller & service test
    
    * DTO is there bont wont pass tru the controller
    
    * get basketController works and sum is correct
    
    * refactor
    
    ---------
    
    Co-authored-by: Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
    Co-authored-by: Emil Sivertsson <Emil.sivertsson@gmail.com>
    Co-authored-by: Cristoffer <119351534+Cristoffer85@users.noreply.github.com>
    
    * starting order object
    
    * added Id to products in basket
    
    ---------
    
    Co-authored-by: Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
    Co-authored-by: Emil Sivertsson <Emil.sivertsson@gmail.com>
    Co-authored-by: Cristoffer <119351534+Cristoffer85@users.noreply.github.com>

---
+++ shopBackend/src/main/java/com/example/shopbackend/model/OrderDTO.java
@@ -0,0 +1,6 @@
+package com.example.shopbackend.model;
+
+public class OrderDTO {
+
+    
+}
--- shopBackend/src/main/java/com/example/shopbackend/service/OrderService.java
+++ shopBackend/src/main/java/com/example/shopbackend/service/OrderService.java
@@ -1,4 +1,31 @@
 package com.example.shopbackend.service;
 
+import com.example.shopbackend.entity.Order;
+import com.example.shopbackend.model.OrderDTO;
+import com.example.shopbackend.repository.OrderQtyRepository;
+import com.example.shopbackend.repository.OrderRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    private final OrderQtyRepository orderQtyRepository;
+
+    public OrderService(OrderRepository orderRepository, OrderQtyRepository orderQtyRepository) {
+        this.orderRepository = orderRepository;
+        this.orderQtyRepository = orderQtyRepository;
+    }
+
+    public List<Object> findAllUserOrders(Long userId) {
+
+        Optional<List<Order>> orders = orderRepository.findByUserId(userId);
+// todo continue here
+        OrderDTO userOrders = new OrderDTO();
+
+        return null;
+    }
+
 }
