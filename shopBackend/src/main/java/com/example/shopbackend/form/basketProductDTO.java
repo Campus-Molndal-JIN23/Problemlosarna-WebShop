04b0e4f5398294d89ef16f6002d6683018e87f22
Kristian Karlson <sppdc1@gmail.com>
2023-12-13 13:26:06 +0100

    OrderDTO in place get will fetch a predefined object. DTO for changing products (Create Update Delete) in basket (#36)
    * OrderDTO in place get will fetch a predefined object
    
    * DTO for changing products (Create Update Delete) in basket

---
+++ shopBackend/src/main/java/com/example/shopbackend/form/basketProductDTO.java
@@ -0,0 +1,7 @@
+package com.example.shopbackend.form;
+
+public record basketProductDTO(
+        Long productId,
+        int quantity
+) {
+}
--- shopBackend/src/test/java/com/example/shopbackend/controller/AuthControllerTest.java
+++ shopBackend/src/test/java/com/example/shopbackend/controller/AuthControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(AuthController.class)
+@SpringBootTest
 @AutoConfigureMockMvc
 class AuthControllerTest {
 
