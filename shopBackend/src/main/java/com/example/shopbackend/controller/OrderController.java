6a1a48ef7b6f56b53df8e24156e30541ee8aed86
Wakana <83waksug@gafe.molndal.se>
2023-12-14 11:31:33 +0100

    OrderController ALLorder

--- shopBackend/src/main/java/com/example/shopbackend/controller/OrderController.java
+++ shopBackend/src/main/java/com/example/shopbackend/controller/OrderController.java
@@ -1,11 +1,8 @@
 package com.example.shopbackend.controller;
 
 
-import com.example.shopbackend.entity.BasketOld;
-import com.example.shopbackend.entity.OrderOld;
-import com.example.shopbackend.entity.ProductOld;
+import com.example.shopbackend.entity.*;
 import com.example.shopbackend.form.UserTest;
-import com.example.shopbackend.entity.User;
 import com.example.shopbackend.model.OrderDTO;
 import com.example.shopbackend.service.OrderService;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/webshop")
@@ -51,10 +50,12 @@ public class OrderController {
     @GetMapping("/orders")
     public ResponseEntity<Object> getOrders() {
 
+
+        OrderDTO orders = orderService.findAllOrders();
         HashMap<String, ArrayList<UserTest>> users = new HashMap<>();
         users.put("users", getUserData());
 
-        return ResponseEntity.ok(users);
+        return ResponseEntity.ok(orders);
     }
 
     private ArrayList<UserTest> getUserData() {
--- shopBackend/src/main/java/com/example/shopbackend/repository/OrderRepository.java
+++ shopBackend/src/main/java/com/example/shopbackend/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.example.shopbackend.repository;
 
 import com.example.shopbackend.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,5 +16,9 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     Optional<List<Order>> getByUserIdAndActiveBasket(Long id, Boolean active);
 
     Optional<List<Order>> findByUserId(Long id);
+
+
+
+    Optional<List<Order>> findAllByActiveBasket(Boolean active);
 
 }
