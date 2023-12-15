cf0b300b44dfac9c81460a6628eb92e00a2e6d05
Kristian Karlson <83krikar@gafe.molndal.se>
2023-12-15 15:28:30 +0100

    user controller in place

--- shopBackend/src/main/java/com/example/shopbackend/controller/UserController.java
+++ shopBackend/src/main/java/com/example/shopbackend/controller/UserController.java
@@ -1,41 +1,23 @@
 package com.example.shopbackend.controller;
 
+import com.example.shopbackend.model.UserDTO;
+import com.example.shopbackend.security.ExtractData;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController("webshop/user")
 public class UserController {
 
-    /* /webshop/
-GET /user/:id
+    private final ExtractData extractData;
 
-Returns the specified user.
-
-    URL Params
-    Data Params
-    Headers
-    Content-Type: application/json
-    Authorization: Bearer <OAuth Token>
-    Success Response:
-    Code: 200
-    Content: { <name och lösenord i objekt> }
-    Error Response:
-        Code: 404
-        Content: { error : "User doesn't exist" }
-        OR
-        Code: 401
-        Content: { error : error : "You are unauthorized to make this request." }
-     */
+    public UserController(ExtractData extractData) {
+        this.extractData = extractData;
+    }
 
     @GetMapping("/")
-    public ResponseEntity<?> userDetails(Principal principal) {
-
-        principal.getName();
-
-
-        return null;
+    public ResponseEntity<?> userDetails(@RequestBody String jwt) {
+        return ResponseEntity.ok(new UserDTO(extractData.getUserID(jwt), extractData.getUserName(jwt), extractData.getUserName(jwt)));
     }
 }
