88f7e39c0b7e509e6ca1848127e1edbe4df38651
Wakana <83waksug@gafe.molndal.se>
2023-12-15 11:59:34 +0100

    får error på order

--- shopBackend/src/main/java/com/example/shopbackend/ShopBackendApplication.java
+++ shopBackend/src/main/java/com/example/shopbackend/ShopBackendApplication.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.*;
-
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // Exclude to make sure the project runs for all.
 //@SpringBootApplication
 public class ShopBackendApplication {
