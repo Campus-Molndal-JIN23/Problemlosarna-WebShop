bed404a2c43312c4c651eaa975b4b6db338c5d6b
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2023-12-17 17:48:50 +0100

    Removed previous try with RandomGif-implementation,
    Added OnMouseOver/Out effect for all links in top navbar, and a little Shoppingcartlogo for basketlink as well.

--- ShopFrontend/src/main/resources/static/script.js
+++ ShopFrontend/src/main/resources/static/script.js
@@ -22,55 +22,19 @@ function closeWindow() {
     window.close();
 }
 
+    <!-- onMouseOver/Out effekt för samtliga länkar i top nav-bar -->
+document.addEventListener("DOMContentLoaded", function () {
+    // Get all the text links in the top of the template
+    const links = document.querySelectorAll('.navbar-nav a.nav-link');
 
-// --------- Functions below, for fantastic text + random gif appearing ---------
+    // Add event listeners for mouseover and mouseout
+    links.forEach(function (link) {
+        link.addEventListener('mouseover', function () {
+            this.style.fontWeight = 'bold';
+        });
 
-// Array of GIFs (from static folder)
-const gifPaths = [
-    "/GifBunny.gif",
-    "/GifMario.gif",
-    "/GifMcZombie.gif",
-    "/GifSonic.gif",
-
-];
-
-// Function to set initial position for the GIF and text at the bottom of the page
-function setInitialPosition() {
-    const gif = document.getElementById("randomGif");
-    const text = document.getElementById("movingText");
-    const screenWidth = window.innerWidth;
-    const screenHeight = window.innerHeight;
-
-    gif.style.top = screenHeight - 100 + "px"; // Adjust the height as needed
-    gif.style.left = -100 + "px";
-
-    text.style.top = screenHeight - 100 + "px"; // Adjust the height as needed
-    text.style.left = -200 + "px"; // Adjust the initial position of the text
-
-    // Randomly select a GIF path
-    const randomGifPath = gifPaths[Math.floor(Math.random() * gifPaths.length)];
-    gif.src = randomGifPath;
-}
-
-// Function to animate the GIF and text
-function moveGifAndText() {
-    const gif = document.getElementById("randomGif");
-    const text = document.getElementById("movingText");
-    const screenWidth = window.innerWidth;
-
-    // Move the GIF and text to the right
-    gif.style.left = parseInt(gif.style.left) + 1 + "px"; // Adjust the speed as needed
-    text.style.left = parseInt(text.style.left) + 1 + "px"; // Adjust the speed as needed
-
-    // Check if the GIF is out of bounds
-    if (parseInt(gif.style.left) > screenWidth) {
-        setInitialPosition(); // Set new random GIF and initial position when out of bounds
-    }
-
-    // Repeat the animation
-    requestAnimationFrame(moveGifAndText);
-}
-
-// Set initial position and start the animation
-setInitialPosition();
-moveGifAndText();
+        link.addEventListener('mouseout', function () {
+            this.style.fontWeight = 'normal';
+        });
+    });
+});
