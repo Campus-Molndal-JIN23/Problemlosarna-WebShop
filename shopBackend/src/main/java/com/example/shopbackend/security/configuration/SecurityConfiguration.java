22bdbefe400028f1ca271823f8758b0ea2e65d75
Kristian Karlson <83krikar@gafe.molndal.se>
2023-12-18 14:10:00 +0100

    Permits all

--- shopBackend/src/main/java/com/example/shopbackend/security/configuration/SecurityConfiguration.java
+++ shopBackend/src/main/java/com/example/shopbackend/security/configuration/SecurityConfiguration.java
@@ -27,9 +27,6 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final UserService userService;
-
     private final static String USER_WHITE_LIST = "webshop/user**";
     private final static String PRODUCT_WHITE_LIST = "webshop/products*";
     private final static String ADMIN_PRODUCT_WHITE_LIST = "webshop/products**";
@@ -37,40 +34,43 @@ public class SecurityConfiguration {
     private final static String BASKET_WHITE_LIST = "webshop/basket**";
     private final static String USER_ORDER_WHITE_LIST = "webshop/order**";
     private final static String ADMIN_ORDER_WHITE_LIST = "webshop/orders**";
-
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final UserService userService;
 
     @Bean //TODO check rights
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(authz -> authz
                         // In memory database used during development
                         .requestMatchers("/h2-console/**").permitAll()
+
                         // Allow public access to authentication-related endpoints
                         .requestMatchers(AUTHORIZATION_WHITE_LIST).permitAll()
 //                        .requestMatchers(HttpMethod.GET,"/webshop/products/**").permitAll()
                         // Access to the Basket for all logged in
-                        .requestMatchers(HttpMethod.GET, BASKET_WHITE_LIST).hasAnyRole("USER","ADMIN")
-                        .requestMatchers(HttpMethod.POST, BASKET_WHITE_LIST).hasAnyRole("USER","ADMIN")
-                        .requestMatchers(HttpMethod.PUT, BASKET_WHITE_LIST).hasAnyRole("USER","ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, BASKET_WHITE_LIST).hasAnyRole("USER","ADMIN")
+                        .requestMatchers(HttpMethod.GET, BASKET_WHITE_LIST).hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, BASKET_WHITE_LIST).hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.PUT, BASKET_WHITE_LIST).hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, BASKET_WHITE_LIST).hasAnyRole("USER", "ADMIN")
                         // Access to Products based on authority
                         .requestMatchers(HttpMethod.GET, PRODUCT_WHITE_LIST).permitAll()
-                        .requestMatchers(HttpMethod.GET, ADMIN_PRODUCT_WHITE_LIST).hasRole("ADMIN") // admin can look at deleted items
+                        .requestMatchers(HttpMethod.GET, PRODUCT_WHITE_LIST).hasAnyRole("USER", "ADMIN")
                         .requestMatchers(HttpMethod.POST, ADMIN_PRODUCT_WHITE_LIST).hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, ADMIN_PRODUCT_WHITE_LIST).hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, ADMIN_PRODUCT_WHITE_LIST).hasRole("ADMIN")
                         // Access to User for all logged in
-                        .requestMatchers(HttpMethod.GET, USER_WHITE_LIST).hasAnyRole("USER","ADMIN")
+                        .requestMatchers(HttpMethod.GET, USER_WHITE_LIST).hasAnyRole("USER", "ADMIN")
                         // Access to Order based on authority
-                        .requestMatchers(HttpMethod.GET, USER_ORDER_WHITE_LIST).hasAnyRole("USER","ADMIN")
-                        .requestMatchers(HttpMethod.POST, USER_ORDER_WHITE_LIST).hasAnyRole("USER","ADMIN")
+                        .requestMatchers(HttpMethod.GET, USER_ORDER_WHITE_LIST).hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, USER_ORDER_WHITE_LIST).hasAnyRole("USER", "ADMIN")
                         .requestMatchers(HttpMethod.GET, ADMIN_ORDER_WHITE_LIST).hasRole("ADMIN")
                         // Require authentication for any other endpoint */
-                        .anyRequest().authenticated())
-                         .headers((headers) ->        //TODO remove headers added just for using H2
+                        .anyRequest().permitAll()) // todo remove
+//                        .anyRequest().authenticated())  // TODO Activate
+                .headers((headers) ->        //TODO remove headers added just for using H2
                         headers
                                 .frameOptions((frameOptions) -> frameOptions.disable())
-                        )
+                )
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
                 // Configure JWT authentication filter
