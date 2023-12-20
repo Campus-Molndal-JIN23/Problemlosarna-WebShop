bd3f067a3cebe2b8a4b9df7fe2805cb31951e58e
Kristian Karlson <83krikar@gafe.molndal.se>
2023-12-20 12:06:27 +0100

    updating sfc

--- shopBackend/src/main/java/com/example/shopbackend/security/configuration/SecurityConfiguration.java
+++ shopBackend/src/main/java/com/example/shopbackend/security/configuration/SecurityConfiguration.java
@@ -19,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -28,12 +29,16 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfiguration {
 
     private final static String USER_WHITE_LIST = "webshop/user**";
-    private final static String PRODUCT_WHITE_LIST = "webshop/products*";
-    private final static String ADMIN_PRODUCT_WHITE_LIST = "webshop/products**";
-    private final static String AUTHORIZATION_WHITE_LIST = "/webshop/auth/**";
-    private final static String BASKET_WHITE_LIST = "webshop/basket**";
-    private final static String USER_ORDER_WHITE_LIST = "webshop/order**";
-    private final static String ADMIN_ORDER_WHITE_LIST = "webshop/orders**";
+
+    private final static String[] GET_USER_WHITE_LIST = {"/webshop/basket*", "/webshop/products", ""};
+    private final static String[] GET_ADMIN_WHITE_LIST = {"", ""};
+
+    private final static String PRODUCT_WHITE_LIST = "/webshop/products*"; // ok
+    private final static String ADMIN_PRODUCT_WHITE_LIST = "/webshop/products**";
+
+    private final static String BASKET_WHITE_LIST = "/webshop/basket**"; //ok
+    private final static String USER_ORDER_WHITE_LIST = "/webshop/order";
+    private final static String ADMIN_ORDER_WHITE_LIST = "/webshop/orders**";
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final UserService userService;
 
@@ -43,27 +48,27 @@ public class SecurityConfiguration {
         http.authorizeHttpRequests(authz -> authz
                         // In memory database used during development
                         .requestMatchers("/h2-console/**").permitAll()
-
                         // Allow public access to authentication-related endpoints
-                        .requestMatchers(AUTHORIZATION_WHITE_LIST).permitAll()
+                        .requestMatchers(POST,"/webshop/auth/*").permitAll()
 //                        .requestMatchers(HttpMethod.GET,"/webshop/products/**").permitAll()
                         // Access to the Basket for all logged in
-                        .requestMatchers(HttpMethod.GET, BASKET_WHITE_LIST).hasAnyRole("USER", "ADMIN")
-                        .requestMatchers(HttpMethod.POST, BASKET_WHITE_LIST).hasAnyRole("USER", "ADMIN")
-                        .requestMatchers(HttpMethod.PUT, BASKET_WHITE_LIST).hasAnyRole("USER", "ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, BASKET_WHITE_LIST).hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(GET, "/webshop/basket", "/webshop/products/*", "/webshop/user/", "").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(POST, "/webshop/orders").hasRole("ADMIN")
+                        .requestMatchers(POST, BASKET_WHITE_LIST).hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(PUT, BASKET_WHITE_LIST).hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(DELETE, BASKET_WHITE_LIST).hasAnyRole("USER", "ADMIN")
                         // Access to Products based on authority
-                        .requestMatchers(HttpMethod.GET, PRODUCT_WHITE_LIST).permitAll()
-                        .requestMatchers(HttpMethod.GET, PRODUCT_WHITE_LIST).hasAnyRole("USER", "ADMIN")
-                        .requestMatchers(HttpMethod.POST, ADMIN_PRODUCT_WHITE_LIST).hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, ADMIN_PRODUCT_WHITE_LIST).hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, ADMIN_PRODUCT_WHITE_LIST).hasRole("ADMIN")
+                        .requestMatchers(GET, PRODUCT_WHITE_LIST).permitAll()
+                        .requestMatchers(GET, PRODUCT_WHITE_LIST).hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(POST, ADMIN_PRODUCT_WHITE_LIST).hasRole("ADMIN")
+                        .requestMatchers(PUT, ADMIN_PRODUCT_WHITE_LIST).hasRole("ADMIN")
+                        .requestMatchers(DELETE, ADMIN_PRODUCT_WHITE_LIST).hasRole("ADMIN")
                         // Access to User for all logged in
-                        .requestMatchers(HttpMethod.GET, USER_WHITE_LIST).hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(GET, USER_WHITE_LIST).hasAnyRole("USER", "ADMIN")
                         // Access to Order based on authority
-                        .requestMatchers(HttpMethod.GET, USER_ORDER_WHITE_LIST).hasAnyRole("USER", "ADMIN")
-                        .requestMatchers(HttpMethod.POST, USER_ORDER_WHITE_LIST).hasAnyRole("USER", "ADMIN")
-                        .requestMatchers(HttpMethod.GET, ADMIN_ORDER_WHITE_LIST).hasRole("ADMIN")
+                        .requestMatchers(GET, "webshop/order").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(POST, USER_ORDER_WHITE_LIST).hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(GET, "webshop/orders**").hasRole("ADMIN")
                         // Require authentication for any other endpoint */
                         .anyRequest().permitAll()) // todo remove
 //                        .anyRequest().authenticated())  // TODO Activate
