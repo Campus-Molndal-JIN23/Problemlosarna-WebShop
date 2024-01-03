ddd2e4514b42aa2d723d30c5ac20112caa2e421e
Emilsivertsson <Emil.sivertsson@gmail.com>
2024-01-03 12:01:21 +0100

    started commenting frontend

--- ShopFrontend/src/main/java/com/example/shopfrontend/controller/AdminController.java
+++ ShopFrontend/src/main/java/com/example/shopfrontend/controller/AdminController.java
@@ -16,6 +16,11 @@ import java.util.List;
 
 import static com.example.shopfrontend.controller.IndexController.currentUser;
 
+/** This class is responsible for handling requests from the admin user.
+ * It checks if the user is logged in and if the user has the admin role before
+ * allowing the user to render the admin pages.
+ * it uses a Logger to log the requests and responses to the console.
+ */
 @Slf4j
 @Controller
 public class AdminController {
@@ -23,12 +28,11 @@ public class AdminController {
     private final ProductHttp productHttp;
     private final OrderHttp orderHttp;
 
+    // the following variables are used to redirect the user to the appropriate page
     private final String ADMIN_URL = "/admin";
     private final String UNAUTHORIZED_URL = "/unauthorized";
     private final String ERROR_URL = "/error";
-
     private final String ADMIN_ROLE = "ROLE_ADMIN";
-
     int status = 0;
 
 
@@ -37,13 +41,20 @@ public class AdminController {
         this.orderHttp = orderHttp;
     }
 
+    // This method is responsible for rendering the admin_index page.
+    // it checks if the user is logged in and if the user has the admin role before rendering the page.
+    // if the product list is null or not fetched correctly, the user is redirected to the error page.
+    // if all is correct so far, the user is directed to the admin_index page and the product list is displayed,
+    // along with the username of the logged in user.
     @GetMapping("/admin")
     public String adminIndex(Model model) throws IOException, ParseException {
         if(currentUser.getRole() == null || !currentUser.getRole().contains(ADMIN_ROLE)) {
+            log.info("not authorized");
             return UNAUTHORIZED_URL;
         }
         List<ProductDTO> products = productHttp.getAllProducts();
         if(products == null) {
+            log.info("products not found");
             return "redirect:" + ERROR_URL;
         } else {
             model.addAttribute("products", products);
@@ -52,16 +63,25 @@ public class AdminController {
         }
     }
 
-
+    // This method is responsible for rendering the admin_create_product page.
+    // it checks if the user is logged in and if the user has the admin role before rendering the page.
+    // if all is correct so far, an empty product object is created and the user is directed to the admin_create_product page.
     @GetMapping ("/admin/create_product")
     public String createProductForm(Model model) {
         if(currentUser.getRole() == null || !currentUser.getRole().contains(ADMIN_ROLE)) {
+            log.info("Not authorized");
             return "redirect:" + UNAUTHORIZED_URL;
         }
         ProductDTO product = new ProductDTO();
         model.addAttribute("product", product);
         return "create_product";
     }
+
+    // This method is responsible for posting the product object to the backend.
+    // it checks if the user is logged in and if the user has the admin role before posting the product object.
+    // if the product is posted successfully, the user is redirected to the admin_index page.
+    // if the product is not posted successfully or the user is not logged in or does not have the admin role,
+    // the user is redirected to appropriate page.
 
     @PostMapping("/admin/create_product")
     public String saveProduct(ProductDTO product) throws IOException {
@@ -70,23 +90,32 @@ public class AdminController {
         }
         status = productHttp.createProduct(product,IndexController.currentUser.getToken());
         if (status == 200) {
+            log.info("saveProduct: " + product);
             return "redirect:" + ADMIN_URL;
         }
         if (status == 401 || status == 403) {
+            log.info("not authorized");
             return "redirect:" + UNAUTHORIZED_URL;
         }
         else {
+            log.info("error");
             return "redirect:" + ERROR_URL;
         }
     }
 
+    // This method is responsible for rendering the admin_edit_product page.
+    // it checks if the user is logged in and if the user has the admin role before rendering the page.
+    // if the product is null or not fetched correctly, the user is redirected to the error page.
+    // if all is correct so far, the user is directed to the admin_edit_product page and the product object is displayed.
     @GetMapping("/admin/edit_product/{id}")
     public String updateProductForm(@PathVariable long id ,Model model) throws IOException, ParseException {
         if(currentUser.getRole() == null || !currentUser.getRole().contains(ADMIN_ROLE)) {
+            log.info("not authorized");
             return "redirect:" + UNAUTHORIZED_URL;
         }
         ProductDTO product = productHttp.getProductById(id);
         if(product == null) {
+            log.info("product not found");
             return "redirect:" + ERROR_URL;
         } else {
             model.addAttribute("product", product);
@@ -94,13 +123,20 @@ public class AdminController {
         }
     }
 
+    // This method is responsible for updating the product object in the backend.
+    // it checks if the user is logged in and if the user has the admin role before updating the product object.
+    // if the product is updated successfully, the user is redirected to the admin_index page.
+    // if the product is not updated successfully or the user is not logged in or does not have the admin role,
+    // the user is redirected to appropriate page.
     @PostMapping ("/admin/edit_product/{id}")
     public String updateProduct(@PathVariable long id , @ModelAttribute ProductDTO product) throws IOException, ParseException {
         if(currentUser.getRole() == null || !currentUser.getRole().contains(ADMIN_ROLE)) {
+            log.info("not authorized");
             return "redirect:" + UNAUTHORIZED_URL;
         }
         ProductDTO productToUpdate = productHttp.getProductById(id);
         if (productToUpdate == null) {
+            log.info("product not found");
             return "redirect:" + ERROR_URL;
         }
         productToUpdate.setId(product.getId());
@@ -112,36 +148,53 @@ public class AdminController {
             log.info("updateProduct: " + productToUpdate);
             return "redirect:" + ADMIN_URL;
         } else {
+            log.info("error");
             return "redirect:" + ERROR_URL;
         }
     }
 
+    // This method is responsible for deleting the product object in the backend.
+    // it checks if the user is logged in and if the user has the admin role before deleting the product object.
+    // if the product is deleted successfully, the user is redirected to the admin_index page.
+    // if the product is not deleted successfully or the user is not logged in or does not have the admin role,
+    // the user is redirected to appropriate page.
     @GetMapping("/admin/delete_product/{id}")
     public String deleteProductForm(@PathVariable long id) throws IOException {
         if(currentUser.getRole() == null || !currentUser.getRole().contains(ADMIN_ROLE)) {
+            log.info("not authorized");
             return "redirect:" + UNAUTHORIZED_URL;
         }
         ProductDTO productToDelete = new ProductDTO();
         productToDelete.setId(id);
         status = productHttp.deleteProductById(productToDelete,IndexController.currentUser.getToken());
         if (status == 204) {
+            log.info("deleteProduct: " + productToDelete);
             return "redirect:" + ADMIN_URL;
         }
         if (status == 401 || status == 403) {
+            log.info("not authorized");
             return "redirect:" + UNAUTHORIZED_URL;
         }
         else {
+            log.info("error");
             return "redirect:" + ERROR_URL;
         }
     }
 
+    // This method is responsible for rendering the all_orders page.
+    // it checks if the user is logged in and if the user has the admin role before rendering the page.
+    // if the order list is null or not fetched correctly, the user is redirected to the error page.
+    // if all is correct so far, the user is directed to the all_orders page and the order list is displayed,
+    // along with the username of the logged in user.
     @GetMapping("/admin/all_orders")
     public String getAllOrders(Model model) throws IOException, ParseException {
         if(currentUser.getRole() == null || !currentUser.getRole().contains(ADMIN_ROLE)) {
+            log.info("not authorized");
             return "redirect:" + UNAUTHORIZED_URL;
         }
         OrderDetailsDTO orders = orderHttp.getAllOrdersForAll(IndexController.currentUser.getToken());
         if(orders == null) {
+            log.info("orders not found");
             return "redirect:" + ERROR_URL;
         } else {
             model.addAttribute("pastOrders", orders);
--- ShopFrontend/src/main/java/com/example/shopfrontend/controller/IndexController.java
+++ ShopFrontend/src/main/java/com/example/shopfrontend/controller/IndexController.java
@@ -7,6 +7,7 @@ import com.example.shopfrontend.http.ProductHttp;
 import com.example.shopfrontend.http.UserHttp;
 import com.example.shopfrontend.models.dto.ProductDTO;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.hc.core5.http.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,27 +19,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import java.io.IOException;
 import java.util.List;
 
-@Controller
+/**
+ * This class is responsible for handling requests from the index page.
+ * It uses a Logger to log the requests and responses to the console.
+ */
 
+@Controller
+@Slf4j
 public class IndexController {
 
     private final ProductHttp productHttp;
     private final UserHttp userHttp;
+
+    // this variable is used to store the current user
     public static LoginResponse currentUser = new LoginResponse();
 
+    // the following variables are used to redirect the user to the appropriate page
     private final String ERROR_URL = "/error";
-
     private final String ADMIN_URL = "/admin";
     private final String USER_URL = "/user";
-
     private final String LOGIN_URL = "/login";
-
     private final String REGISTRATION_URL = "/registration";
-
     private static final String INDEX_URL = "/index";
-
-
-
     private final String ADMIN_ROLE = "ROLE_ADMIN";
     private final String USER_ROLE = "ROLE_USER";
     String loginErrorMessage = "";
@@ -49,6 +51,10 @@ public class IndexController {
         this.productHttp = productHttp;
         this.userHttp = userHttp;
     }
+
+    // This method is responsible for rendering the index page and is called when the user navigates to the root url.
+    // its manin purpose is to redirect the user to the index page, if not written the correct URL.
+    // It lists all products and displays them on the index page.
     @GetMapping("")
     public String listProducts(Model model) throws IOException, ParseException {
         List<ProductDTO> products = productHttp.getAllProducts();
@@ -60,10 +66,14 @@ public class IndexController {
         }
     }
 
+    // This method is responsible for rendering the index page and is called when the user navigates to the /index url.
+    // It lists all products and displays them on the index page.
+    // if the product list is null or not fetched correctly, the user is redirected to the error page.
     @GetMapping("/index")
     public String listProductsIndex(Model model) throws IOException, ParseException {
         List<ProductDTO> products = productHttp.getAllProducts();
         if(products == null) {
+            log.info("products not found");
             return "redirect:" + ERROR_URL;
         } else {
             model.addAttribute("products", products);
@@ -71,16 +81,20 @@ public class IndexController {
         }
     }
 
+    // This method is responsible for rendering the view_one_product and displaying the details of the product with the given id.
+    //if the product is not found, the user is redirected to the error page.
     @GetMapping("/index/one/{id}")
     public String getOneProduct(@PathVariable long id, Model model1) throws IOException, ParseException {
         ProductDTO product = productHttp.getProductById(id);
         if(product == null) {
+            log.info("product not found");
             return "redirect:" + ERROR_URL;
         } else {
             model1.addAttribute("product", product);
             return "view_one_product";
         }
     }
+
 
     @GetMapping("/login")
     public String login(Model model) {
