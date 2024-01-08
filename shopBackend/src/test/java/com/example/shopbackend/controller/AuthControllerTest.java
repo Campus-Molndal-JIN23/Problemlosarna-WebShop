26af623b2846345fd29ff9fc46129cb63b30a817
Wakana <83waksug@gafe.molndal.se>
2024-01-08 23:59:54 +0100

    test

--- shopBackend/src/test/java/com/example/shopbackend/controller/AuthControllerTest.java
+++ shopBackend/src/test/java/com/example/shopbackend/controller/AuthControllerTest.java
@@ -4,11 +4,7 @@ import com.example.shopbackend.entity.Role;
 import com.example.shopbackend.entity.Roles;
 import com.example.shopbackend.entity.User;
 import com.example.shopbackend.form.LoginForm;
-import com.example.shopbackend.form.LoginResponseDTO;
-import com.example.shopbackend.repository.RoleRepository;
-import com.example.shopbackend.repository.UserRepository;
 import com.example.shopbackend.service.AuthService;
-import com.example.shopbackend.service.BasketService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -17,25 +13,18 @@ import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import java.lang.reflect.*;
 import java.security.Key;
 import java.util.*;
-import sun.misc.Unsafe;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -53,18 +42,6 @@ class AuthControllerTest {
     private String role;
 
     User user;
-    @Mock
-    private UserRepository userRepository;
-
-
-    @Mock
-    UserRepository mockUserService;
-
-    @Mock
-    RoleRepository mockUserRoleRepository;
-
-    @Mock
-    BasketService mockBasketService;
 
     @Autowired
     PasswordEncoder passwordEncoder;
@@ -74,6 +51,7 @@ class AuthControllerTest {
 
     @Autowired
     MockMvc mvc;
+
     @BeforeEach
     void setUp() throws Exception {
         userName = "user";
@@ -81,6 +59,7 @@ class AuthControllerTest {
         loginForm = new LoginForm(userName,password);
         role = "ROLE_USER";
         Roles roles = new Roles(Role.ROLE_USER);
+
 
         String jwtSigningKey = "0b3a2839ae978330c29d866068171086524b7cd5a7e8a7ddc5fd7a893f7caf7b";
         Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSigningKey));
@@ -104,10 +83,7 @@ class AuthControllerTest {
                 .userName(userName)
                 .password(passwordEncoder.encode(loginForm.getPassword()))
                 .roles(new HashSet<>(Collections.singletonList(roles))).build();
-
-        initMocks(this);
     }
-
 
     @Test
     void register() throws Exception {
@@ -122,7 +98,6 @@ class AuthControllerTest {
                 .andExpect(status().isOk());
 
     }
-
 
 
 }
